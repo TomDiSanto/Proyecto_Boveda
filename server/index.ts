558880df88aa51e9d7515f4f3f2676deec903dda
TomDiSanto <ditodisanto@gmail.com>
2026-07-10 07:19:34 -0300

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,13 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import app from './app';
+// Import dinámico, para forzar que se ejecute DESPUÉS de dotenv.config()
+async function start() {
+  const { default: app } = await import('./app');
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () => {
+    console.log(`Backend server is running on http://localhost:${PORT}`);
+  });
+}
 
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-  console.log(`Backend server is running on http://localhost:${PORT}`);
-});
+start();
